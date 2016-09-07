e89869c7775e2b200bf31310b314cae5dc8edf06
Florian Hofmann <fho@f12n.de>
2016-09-07 15:32:14 +0200

    implemented derived combinators

--- src/Data/Vector/Split/Internal.hs
+++ src/Data/Vector/Split/Internal.hs
@@ -32,14 +32,9 @@ data Chunk v a = Delim (v a) | Text (v a)
 
 type SplitList v a = [Chunk v a]
 
-
-data Splitter a = Splitter {
-    delimiter :: Delimiter a
-}
+type Splitter v a = v a -> SplitList v a
 
 
-{-split :: Vector v a => Splitter a -> v a -> [v a]-}
-{-split splitter v = -}
 
 
 toSplitList :: Vector v a => Delimiter a -> v a -> SplitList v a
@@ -49,6 +44,49 @@ toSplitList d v = go 0
                                       Nothing    -> go (i+1)
                | otherwise      = [Text v]
 
+
+split :: Vector v a => Splitter v a -> v a -> [v a]
+split s v = map go $ s v
+    where go (Text t)  = t
+          go (Delim d) = d
+
+
+
+
+-- Basic Strategies
+
+-- | A splitting strategy that splits on any one of the given elements. For example:
+-- >>> split (oneOf "xyz") "aazbxyzcxd" == ["aa","z","b","x","","y","","z","c","x","d"]
+oneOf :: (Vector v a, Eq a) => v a -> Splitter v a
+oneOf xs = toSplitList delim
+    where delim = Delimiter (BV.singleton (`V.elem` xs))
+
+
+-- | A splitting strategy that splits on the given list, when it is
+-- encountered as an exact subsequence. For example:
+--
+-- >>> split (onSublist "xyz") "aazbxyzcxd" == ["aazb","xyz","cxd"]
+-- Note that splitting on the empty list is a special case, which splits
+-- just before every element of the list being split. For example:
+--
+-- >>> split (onSublist "") "abc" == ["","","a","","b","","c"]
+-- >>> split (dropDelims . dropBlanks $ onSublist "") "abc" == ["a","b","c"]
+--
+-- However, if you want to break a list into singleton elements like this,
+-- you are better off using chunksOf 1, or better yet, map (:[]).
+onSublist :: (Vector v a, Eq a) => v a -> Splitter v a
+onSublist xs = toSplitList delim
+    where delim = Delimiter (V.map (==) . V.convert $ xs)
+
+-- | A splitting strategy that splits on any elements that satisfy the given predicate. For example:
+--
+-- >>> split (whenElt (<0)) [2,4,-3,6,-9,1] == [[2,4],[-3],[6],[-9],[1]]
+whenElt :: (Vector v a) => (a -> Bool) -> Splitter v a
+whenElt p = toSplitList delim
+    where delim = Delimiter (V.singleton p)
+
+
+-- Strategy Transformers (how does haddock work?)
 
 dropDelims :: Vector v a => SplitList v a -> SplitList v a
 dropDelims = filter go 
@@ -71,23 +109,64 @@ condense (rs : rst)                = rs : condense rst
 condense []                        = []
 
 dropInitBlank :: Vector v a => SplitList v a -> SplitList v a
-dropInitBlank (Text a : rst) | V.null a = rst
+dropInitBlank (Text t : rst) | V.null t = rst
 dropInitBlank rst                       = rst
 
 dropFinalBlank :: Vector v a => SplitList v a -> SplitList v a
-dropFinalBlank (rst : [Text _]) = [rst]
+dropFinalBlank (rst : [Text t]) | V.null t = [rst]
 dropFinalBlank (rs : rst)       = rs : dropFinalBlank rst
 dropFinalBlank []               = []
 
 dropInnerBlanks :: Vector v a => SplitList v a -> SplitList v a
-dropInnerBlanks (Delim a : Text t : Delim b : rst) | V.null t  = Delim a : Delim b : dropInnerBlanks rst
+dropInnerBlanks (Delim a : Text t : Delim b : rst) | V.null t  = Delim a : dropInnerBlanks (Delim b : rst)
                                                    | otherwise = Delim a : Text t : dropInnerBlanks (Delim b : rst)
 dropInnerBlanks (rs : rst)                                     = rs : dropInnerBlanks rst
 dropInnerBlanks []                                             = []
 
-{-oneOf :: (Vector v a, Eq a) => BV.Vector a -> v a -> [v a]-}
-{-oneOf ds = L.unfoldr go 0-}
-{-    where delim = Delimiter (`V.elem` ds)-}
-{-          go i  | i < V.length ds = case matchDelim (Delimiter (`V.elem` ds)) of-}
-{-                                      Nothing      -> Just (i+1)-}
-{-                                      (Just (l,r)) ->  -}
+
+-- Derived combinators
+
+-- Drop all blank chunks from the output, and condense consecutive
+-- delimiters into one. 
+-- Equivalent to dropInitBlank . dropFinalBlank . condense. For example:
+--
+-- >>> split (oneOf ":") "::b:::a" == ["",":","",":","b",":","",":","",":","a"]
+-- >>> split (dropBlanks $ oneOf ":") "::b:::a" == ["::","b",":::","a"]
+dropBlanks :: Vector v a => SplitList v a -> SplitList v a
+dropBlanks = condense . dropInitBlank . dropFinalBlank . dropInnerBlanks
+
+-- Make a strategy that splits a list into chunks that all start with the
+-- given subsequence (except possibly the first). Equivalent to
+-- dropInitBlank . keepDelimsL . onSublist. For example:
+--
+-- >>> split (startsWith "app") "applyapplicativeapplaudapproachapple" == ["apply","applicative","applaud","approach","apple"]
+startsWith :: (Vector v a, Eq a) => v a -> Splitter v a
+startsWith xs = dropInitBlank . keepDelimsL . onSublist xs
+
+-- Make a strategy that splits a list into chunks that all start with one
+-- of the given elements (except possibly the first). Equivalent to
+-- dropInitBlank . keepDelimsL . oneOf. For example:
+--
+-- >>> split (startsWithOneOf ['A'..'Z']) "ACamelCaseIdentifier" == ["A","Camel","Case","Identifier"]
+startsWithOneOf :: (Vector v a, Eq a) => v a -> Splitter v a
+startsWithOneOf xs = dropInitBlank . keepDelimsL . oneOf xs
+
+-- Make a strategy that splits a list into chunks that all end with the
+-- given subsequence, except possibly the last. Equivalent to
+-- dropFinalBlank . keepDelimsR . onSublist. For example:
+--
+-- >>> split (endsWith "ly") "happilyslowlygnarlylily" == ["happily","slowly","gnarly","lily"]
+endsWith :: (Vector v a, Eq a) => v a -> Splitter v a
+endsWith xs = dropFinalBlank . keepDelimsR . onSublist xs
+
+
+
+-- Note: this function is not consistent with the Data.List.Split version
+--
+-- Make a strategy that splits a list into chunks that all end with one of
+-- the given elements, except possibly the last. Equivalent to
+-- dropFinalBlank . keepDelimsR . oneOf. For example:
+--
+-- >>> split (condense $ endsWithOneOf ".,?! ") "Hi, there!  How are you?" == ["Hi, ","there!  ","How ","are ","you?"]
+endsWithOneOf :: (Vector v a, Eq a) => v a -> Splitter v a
+endsWithOneOf xs = dropFinalBlank . keepDelimsR . oneOf xs
